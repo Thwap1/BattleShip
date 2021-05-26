3626d365a9dce53d933ec69e805525d704b05315
Thwap1 <kuisma.sami@gmail.com>
2021-05-26 15:44:26 +0300

    Add project files.

---
+++ Source/BattleShip/BattleShipGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleShipGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLESHIP_API ABattleShipGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
